e5b4a1385a06d64ea2785fc188a2330e603a54ef
Himanshu <sinhah239@gmail.com>
2022-12-22 20:20:14 -0700

    it does the graphing, any size more than 100 x 100 does break it tho (n^2 complexity)

--- src/Grapher.java
+++ src/Grapher.java
@@ -9,46 +9,85 @@ public class Grapher {
 
     private int coef[];
 
-    public Grapher(int n, int m, int deg) {
+    public Grapher(int n, int m) {
         this.n = n;
         this.m = m;
-        this.coef = new int[deg + 1];
 
         image = new BitImage(this.n, this.m);
     }
 
     public void setEquation() {
         Scanner scan = new Scanner(System.in);
+        System.out.print("Set equation degree: ");
+        coef = new int[Integer.parseInt(scan.nextLine()) + 1];
         for (int i = 0; i < coef.length; i++) {
-            System.out.print("Set the coefficient for the " + i + "th degree: ");
+            if (i % 10 == 1 && i != 11) {
+                System.out.print("Set the coefficient for the " + i + "st degree: ");
+            } else if (i % 10 == 2 && i != 12) {
+                System.out.print("Set the coefficient for the " + i + "nd degree: ");
+            } else if (i % 10 == 3 && i != 13) {
+                System.out.print("Set the coefficient for the " + i + "rd degree: ");
+            } else {
+                System.out.print("Set the coefficient for the " + i + "th degree: ");
+            }
             coef[i] = Integer.parseInt(scan.nextLine());
         }
-        System.out.println(Arrays.toString(coef));
     }
 
     public String getEquation() {
-        String equation = "";
+        String equation = "y = ";
         for (int i = coef.length - 1; i >= 0; i--) {
             if (coef[i] > 0) {
                 if (i == coef.length - 1) {
-                    equation += "" + coef[i] + "x^" + i;
+                    equation += "" + (coef[i] == 1 ? "" : coef[i]) + (i == 1 ? "x" : ("x^" + i));
                 } else if (i == 0){
-                    equation += " + " + coef[i];
+                    equation += " + " + (coef[i] == 1 ? "1" : coef[i]);
                 } else {
-                    equation += " + " + coef[i] + "x^" + i;
+                    equation += " + " + (coef[i] == 1 ? "" : coef[i]) + (i == 1 ? "x" : ("x^" + i));
                 }
             } else if (coef[i] < 0) {
                 if (i == coef.length - 1) {
-                    equation += "-" + (coef[i]*-1) + "x^" + i;
+                    equation += "-" + (coef[i] == -1 ? "" : (coef[i]*-1)) + "x^" + (i == 1 ? "x" : ("x^" + i));
                 } else if (i == 0){
-                    equation += " - " + (coef[i]*-1);
+                    equation += " - " + (coef[i] == -1 ? "1" : (coef[i]*-1));
                 } else {
-                    equation += " - " + (coef[i]*-1) + "x^" + i;
+                    equation += " - " + (coef[i] == -1 ? "" : (coef[i]*-1)) + (i == 1 ? "x" : ("x^" + i));
                 }
             }
         }
-
         return equation;
+    }
+
+    public int calculateY(int x) {
+        int y = 0;
+        for (int i = 0; i < coef.length; i++) {
+            y += (coef[i] * Math.pow(x, i));
+        }
+        return y;
+    }
+
+    public int calculate() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter x value: ");
+        int x = Integer.parseInt(scan.nextLine());
+        int y = calculateY(x);
+        System.out.println("y = " + y);
+        return y;
+    }
+
+    public void setGraph() {
+        for (int i = image.N() - 1; i >= 0; i--) {
+            for (int j = 0; j < image.M(); j++) {
+                if ((image.N() - 1 - i) == calculateY(j))
+                    image.setPixel(i, j, true);
+                else
+                    image.setPixel(i, j, false);
+            }
+        }
+    }
+
+    public void printGraph() {
+        System.out.println(image);
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Grapher graph = new Grapher(10, 10, 4);
+        Grapher graph = new Grapher(100, 100);
         graph.setEquation();
         System.out.println(graph.getEquation());
+        graph.setGraph();
+        graph.printGraph();
     }
 }
