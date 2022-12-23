6bd9b8a1d6326445a5b39dd5f92196c56e213414
Himanshu <sinhah239@gmail.com>
2022-12-22 19:21:48 -0700

    started equation processing

--- src/Grapher.java
+++ src/Grapher.java
@@ -1,9 +1,54 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Grapher {
 
     private BitImage image;
     private int n;
     private int m;
 
-    public Grapher
+    private int coef[];
+
+    public Grapher(int n, int m, int deg) {
+        this.n = n;
+        this.m = m;
+        this.coef = new int[deg + 1];
+
+        image = new BitImage(this.n, this.m);
+    }
+
+    public void setEquation() {
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < coef.length; i++) {
+            System.out.print("Set the coefficient for the " + i + "th degree: ");
+            coef[i] = Integer.parseInt(scan.nextLine());
+        }
+        System.out.println(Arrays.toString(coef));
+    }
+
+    public String getEquation() {
+        String equation = "";
+        for (int i = coef.length - 1; i >= 0; i--) {
+            if (coef[i] > 0) {
+                if (i == coef.length - 1) {
+                    equation += "" + coef[i] + "x^" + i;
+                } else if (i == 0){
+                    equation += " + " + coef[i];
+                } else {
+                    equation += " + " + coef[i] + "x^" + i;
+                }
+            } else if (coef[i] < 0) {
+                if (i == coef.length - 1) {
+                    equation += "-" + (coef[i]*-1) + "x^" + i;
+                } else if (i == 0){
+                    equation += " - " + (coef[i]*-1);
+                } else {
+                    equation += " - " + (coef[i]*-1) + "x^" + i;
+                }
+            }
+        }
+
+        return equation;
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Grapher graph = new Grapher(10, 10, 4);
+        graph.setEquation();
+        System.out.println(graph.getEquation());
     }
 }
